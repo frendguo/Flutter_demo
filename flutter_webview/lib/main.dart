72b54989286a3c5cffb9e6f64dd5b39618143cb6
frendguo <frendguo@live.cn>
2020-10-14 15:35:08 +0800

    修改flutter_webview

--- flutter_webview/lib/main.dart
+++ flutter_webview/lib/main.dart
@@ -31,7 +31,7 @@ class _WebViewDemoState extends State<WebViewDemo> {
       body: Builder(builder: (BuildContext context) {
         return InAppWebView(
           // initialFile: "assets/index.html",
-          initialUrl: "http://0.0.0.0:1234/",
+          initialUrl: "http://172.20.211.179:1234/",
           // initialUrl: "http://enow.test.seewo.com/view?version=1&from=remote#/enbx/6e1bcce8-de99-4563-bee1-a8b8a3c87ba8",
           initialHeaders: {},
           onWebViewCreated: (InAppWebViewController controller) {
