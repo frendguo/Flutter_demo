a98db227f7ccc94b4035f8922c5f1db59ce62984
frendguo <frendguo@live.cn>
2020-10-15 15:53:39 +0800

    删除老的flutter-webview demo

--- flutter_webview/android/app/src/main/kotlin/com/cvte/demo/flutter_webview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cvte.demo.flutter_webview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_webview/assets/js/index.js
+++
@@ -1,2 +0,0 @@
-let ele = document.getElementById("root");
-ele.appendChild(document.createTextNode("Hello World"));
